72d9f7861da1aeba32b76f7ee1d782069156be32
Dirgh Kansara <66860423+dirghkansara@users.noreply.github.com>
2020-11-18 15:09:34 +0530

    Add files via upload

---
+++ at.js
@@ -0,0 +1,9 @@
+const baseValue = prompt('Enter the base of the triangle: ');
+const heightValue = prompt('ENter the height of the triangle: ');
+
+// calculate the area 
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    'The area of the triangle is ${areaValue}'
+)
